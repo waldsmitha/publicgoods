76de16055538668c358968cd9d683ce506f53cad
waldsmitha <waldsmitha@gmail.com>
2021-12-18 14:25:17 -0500

    Added additional styling and tweaks. I suppose this is the best I can do in 2 hours. Cheers.

--- js/index.js
+++
@@ -1,6 +0,0 @@
-import { gsap } from "gsap";
-import { TextPlugin } from "gsap/TextPlugin";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(TextPlugin, ScrollToPlugin, ScrollTrigger);
